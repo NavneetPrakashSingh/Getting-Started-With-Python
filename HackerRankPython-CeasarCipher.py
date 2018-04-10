6abae630c3f2693f3ec42375817f997587a1c493
NavneetPrakashSingh <navneet_prakash_singh@live.com>
2018-04-10 20:50:40 +0530

    Program for adding cipher algorithm
    Progarm for adding cipher algorithm

--- HackerRankPython-CeasarCipher.py
+++ HackerRankPython-CeasarCipher.py
@@ -10,13 +10,30 @@ Created on Mon Apr  2 18:59:00 2018
 
 def caesarCipher(s, k):
     finalString = ""
-    for i in s:
-        print("ord of i is",ord(i))
-        finalChar = (ord(i) + k)%26
-        print(finalChar)
-        finalString = finalString+chr(finalChar)
-    print(finalString)        
-    return "reached here"
+    characterValues = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    upperValue = "false"
+    for i in s:   
+        if(i.isupper()):
+            upperValue = "true"
+            currentValue = i.lower()
+            
+        else:
+            currentValue = i
+            
+        if currentValue in characterValues:            
+            characterIndex = characterValues.index(currentValue) + 1            
+            finalChar = (characterIndex + k)%26      
+            
+            if(upperValue == "true"):
+                finalString = finalString+characterValues[finalChar-1].upper()
+                
+                upperValue = "false"
+            else:                
+                finalString = finalString+characterValues[finalChar-1]            
+        else:
+            finalString = finalString+currentValue
+        
+    return finalString
 
 if __name__ == "__main__":
     n = int(input().strip())
